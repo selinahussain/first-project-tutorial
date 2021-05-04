77823f153db52d2c00ccb37bbfe7a6f7a6f66300
selinahussain <selinahussain@live.com>
2021-05-04 14:55:03 +0100

    creating test for update ()

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import models.DataModel
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
@@ -12,6 +14,9 @@ import repositories.DataRepository
 import scala.concurrent.{ExecutionContext, Future}
 import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
+import play.api.libs.json.{JsObject, Json}
+import reactivemongo.api.commands.{LastError, WriteResult}
+import reactivemongo.core.errors.GenericDriverException
 
 class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest with MockitoSugar {
 
@@ -19,6 +24,9 @@ class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest with Mo
 
   implicit lazy val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val mockDataRepository: DataRepository = mock[DataRepository]
+
+  implicit val system: ActorSystem = ActorSystem("Sys")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   object TestApplicationController extends ApplicationController(
     controllerComponents,
@@ -36,6 +44,7 @@ class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest with Mo
   "ApplicationController .index" when {
 
     "return OK" in {
+
       when(mockDataRepository.find(any())(any()))
         .thenReturn(Future(List(dataModel)))
 
@@ -45,17 +54,93 @@ class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest with Mo
     }
   }
 
-  "ApplicationController .create()" should {
+  "ApplicationController .create" when {
+
+    "the json body is valid" should {
+
+
+      "return Created" in {
+        val jsonBody: JsObject = Json.obj(
+          "_id" -> "abcd",
+          "name" -> "test name",
+          "description" -> "test description",
+          "numSales" -> 100
+        )
+
+        val writeResult: WriteResult = LastError(ok = true, None, None, None, 0, None, updatedExisting = false, None, None, wtimeout = false, None, None)
+
+        when(mockDataRepository.create(any()))
+          .thenReturn(Future(writeResult))
+
+        val result = TestApplicationController.create()(FakeRequest().withBody(jsonBody))
+
+        status(result) shouldBe Status.CREATED
+      }
+    }
+
+    "the json body is not valid" should {
+
+      "return BAD_REQUEST" in {
+        val jsonBody: JsObject = Json.obj(
+          "_id" -> "abcd",
+          "fail" -> "test name"
+        )
+
+        val result = TestApplicationController.create()(FakeRequest().withBody(jsonBody))
+
+        status(result) shouldBe Status.BAD_REQUEST
+      }
+
+    }
 
   }
+
 
   "ApplicationController .read()" should {
 
   }
 
-  "ApplicationController .update()" should {
+  "ApplicationController.update()" when {
 
+    "supplied valid json" should {
+
+      val jsonBody: JsObject = Json.obj(
+        "_id" -> "abcd",
+        "name" -> "test name",
+        "description" -> "test description",
+        "numSales" -> 100
+      )
+
+      "return a <status code for success>" in {
+
+        when(mockDataRepository.update(dataModel))
+          .thenReturn(Future(dataModel))
+
+        val result = TestApplicationController.update("_id": String)(FakeRequest().withBody(jsonBody))
+
+        status(result) shouldBe Status.ACCEPTED
+      }
+
+      "return the correct JSON body" in {
+        val result = TestApplicationController.update("_id": String)(FakeRequest().withBody(jsonBody))
+        await(jsonBodyOf(result)) shouldBe jsonBody
+      }
+    }
+
+    "supplied invalid json" should {
+      val jsonBody: JsObject = Json.obj(
+        "unexpected field" -> "foo"
+      )
+
+      "return a <status code for failure>" in {
+        val result = TestApplicationController.update("_id": String)(FakeRequest().withBody(jsonBody))
+
+        status(result) shouldBe Status.BAD_REQUEST
+      }
+    }
   }
+
+
   "ApplicationController .delete()" should {
 
   }
